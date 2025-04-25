0c00a45990dff983d9d1933a042d529dded39da5
Charte Erbeth <charte_charte@proton.me>
2025-04-25 07:28:04 -0400

    #2 #3 (feature) Creación de strings aleatorios según un tamaño determinado.
    Primer paso del main:
    - Se ejecuta la creación de un archivo aleatorio de 10MB si se llama el comando sin argumentos.

--- build-random-files.lisp
+++ build-random-files.lisp
@@ -12,3 +12,33 @@ Using the `filename' and the `size'."
 		 (dotimes (i chunk)
 		   (setf (aref buffer i) (random 256)))
 		 (write-sequence buffer output-stream :end chunk))))))
+
+(defun random-string (len-chars)
+  "Genera strings aleatorios, dado un tamaño `len-chars'."
+  (let ((chars (concatenate 'string "abcdefghijklmnopqrstuvwxyz"
+			    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			    "0123456789")))
+    (labels ((random-element (lst)
+	       (aref lst (random (length lst))))
+	     (random-str (len-chars)
+	       (if (zerop len-chars)
+		   nil
+		   (cons (random-element chars)
+			 (random-str (1- len-chars))))))
+      (concatenate 'string (random-str len-chars)))))
+
+(defun main ()
+  (let ((args (si:command-args)))
+    (cond
+      ((= (length args) 1)
+       (create-random-file
+	(concatenate 'string (random-string 25) ".bin")
+	(* 10 1024 1024)))))
+  (ext:quit 0))
+
+(main)
+(si:top-level
+ (lambda ()
+   (unwind-protect
+	(main)
+     (ext:quit 0))))
